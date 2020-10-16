4b9d926a877ab8851502a09bc868624573123e38
Arda Büyükkaya <42712921+whichbuffer@users.noreply.github.com>
2020-10-16 21:27:50 +0300

    Todo App

---
+++ README.md
@@ -0,0 +1,11 @@
+
+```bash
+# Install dependencies
+npm install
+
+# Serve on localhost:8080
+npm run serve
+
+# Build for production
+npm run build
+```
---
+++ src/views/About.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="about">
+    <h1>About</h1>
+    <br>
+    <p>This is the TodoList app for learning VueJS</p>
+  </div>
+</template>
